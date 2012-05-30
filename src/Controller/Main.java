6400d70374f518f1753b1473017c1b6888664ebf
Guilherme Bonfim <guilhermemmb@gmail.com>
2012-05-30 01:38:50 +0000

    commitei um teste. removi.

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -32,16 +32,16 @@ public class Main {
 		//fazendo a avaliação das densidades das bombas
 		//Field.printGameInConsole(mapper);
 		
-		//testando a remocao de bombas
-		if(Field.removeBomb(mapper, Field.bombPosition.get(1)))
-			System.out.println("Removi a bomba de posicao "+Field.bombPosition.get(1).getX() +" " + Field.bombPosition.get(1).getY());
-		
-		if(Field.removeBomb(mapper, Field.bombPosition.get(4)))
-			System.out.println("Removi a bomba de posicao "+Field.bombPosition.get(4).getX() +" " + Field.bombPosition.get(4).getY());
-		
-		if(Field.removeBomb(mapper, Field.bombPosition.get(6)))
-			System.out.println("Removi a bomba de posicao "+Field.bombPosition.get(6).getX() +" " + Field.bombPosition.get(6).getY());
-		
+//		//testando a remocao de bombas
+//		if(Field.removeBomb(mapper, Field.bombPosition.get(1)))
+//			System.out.println("Removi a bomba de posicao "+Field.bombPosition.get(1).getX() +" " + Field.bombPosition.get(1).getY());
+//		
+//		if(Field.removeBomb(mapper, Field.bombPosition.get(4)))
+//			System.out.println("Removi a bomba de posicao "+Field.bombPosition.get(4).getX() +" " + Field.bombPosition.get(4).getY());
+//		
+//		if(Field.removeBomb(mapper, Field.bombPosition.get(6)))
+//			System.out.println("Removi a bomba de posicao "+Field.bombPosition.get(6).getX() +" " + Field.bombPosition.get(6).getY());
+//		
 		//Field.printGameInConsole(mapper);
 		
 		Field.printGame(mapper);
